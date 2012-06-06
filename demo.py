f48a2dd6fb0e58fb464a9d90b9e6b953fb3d298b
Denver Coneybeare <denver@sleepydragon.org>
2012-06-05 21:55:43 -0400

    shortened demo.py

--- demo.py
+++ demo.py
@@ -1,13 +1,8 @@
-def main():
-    print("Hello, welcome")
-    with open(__file__, "rt") as f:
-        for line in f:
-            handle_line(line)
-    return 0
-
 def handle_line(line):
     line_stripped = line.rstrip()
     print(line_stripped)
 
-if __name__ == "__main__":
-    main()
+print("Hello, welcome")
+with open(__file__, "rt") as f:
+    for line in f:
+        handle_line(line)
